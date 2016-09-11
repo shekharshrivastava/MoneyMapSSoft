3a89b4f819d86ef4b9977d40f5463d4cf667be03
shekharshrivastava <ershrivastavashekhar@gmail.com>
2016-09-11 10:46:09 +0530

    added icons for drawer in home activity

--- ElectroKart/app/src/main/java/com/example/shasha/electrokart/Ui/HomeActivity.java
+++ ElectroKart/app/src/main/java/com/example/shasha/electrokart/Ui/HomeActivity.java
@@ -212,7 +212,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         mSpendAmountDisplay = (TextView) findViewById(R.id.spendAmountDisplay);
 
         String TITLES[] = {"Notification", "Settings", "Help", "Info"};
-        int ICONS[] = {R.drawable.notification, R.drawable.setting, R.drawable.help, R.drawable.info};
+        int ICONS[] = {R.drawable.notifications_drawer, R.drawable.settings_drawer, R.drawable.help_drawer, R.drawable.info_drawer};
         mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
 
         mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
